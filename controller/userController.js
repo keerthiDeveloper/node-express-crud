9a4e3ba0aa6d9dce82682702e747d582f4d6eb2c
keerthideveloper <keerthifullstackdeveloper>
2022-12-12 19:58:07 +0530

    try catch added error handler

--- controller/userController.js
+++ controller/userController.js
@@ -8,86 +8,125 @@ const userController = {};
 // Get All User Data
 
 userController.getAllUser = (req, res) => {
-  User.find((err, docs) => {
-    if (!err) {
-      res.send({ message: "success", data: docs });
-    } else {
-      res.send({ message: "Failed", data: "", error: err.message });
-    }
-  });
+  try {
+    User.find((err, docs) => {
+      if (!err) {
+        res.send({ message: "success", data: docs });
+      } else {
+        res.send({ message: "Failed", data: "", error: err.message });
+      }
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error?.message,
+    });
+  }
 };
 
 // Save User
 
 userController.saveUser = (req, res) => {
-  const user = new User({
-    name: req.body.name,
-    mobileNo: req.body.mobileNo,
-    email: req.body.email,
-    location: req.body.location,
-    pinCode: req.body.pinCode,
-  });
-  user.save((err, doc) => {
-    if (!err) {
-      res.send({ message: "success", data: doc });
-    } else {
-      res.send({ message: "Failed", data: "", error: err.message });
-    }
-  });
-};
-
-// get User
-
-userController.getSingleUser = (req, res) => {
-  if (!ObjectId.isValid(req.params.id))
-    return res.status(400).send(`No record with given id:${req.params.id}`);
-  User.findById(req.params.id, (err, doc) => {
-    if (!err) {
-      res.send({ message: "success", data: doc });
-    } else {
-      res.send({ message: "Failed", data: "", error: err.message });
-    }
-  });
-};
-
-// Update User
-
-userController.updateUser = (req, res) => {
-  if (!ObjectId.isValid(req.params.id))
-    return res.status(400).send(`user not match id:${req.params.id}`);
-  const user = {
-    name: req.body.name,
-    mobileNo: req.body.mobileNo,
-    email: req.body.email,
-    location: req.body.location,
-    pinCode: req.body.pinCode,
-  };
-  User.findByIdAndUpdate(
-    req.params.id,
-    { $set: user },
-    { new: true },
-    (err, doc) => {
+  try {
+    const user = new User({
+      name: req.body.name,
+      mobileNo: req.body.mobileNo,
+      email: req.body.email,
+      location: req.body.location,
+      pinCode: req.body.pinCode,
+    });
+    user.save((err, doc) => {
       if (!err) {
         res.send({ message: "success", data: doc });
       } else {
         res.send({ message: "Failed", data: "", error: err.message });
       }
-    }
-  );
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error?.message,
+    });
+  }
+};
+
+// get User
+
+userController.getSingleUser = (req, res) => {
+  try {
+    if (!ObjectId.isValid(req.params.id))
+      return res.status(404).send({
+        message: `No Record with given id:${req.params.id}`,
+        data: "",
+      });
+    User.findById(req.params.id, (err, doc) => {
+      if (!err) {
+        res.send({ message: "success", data: doc });
+      } else {
+        res.send({ message: "Failed", data: "", error: err.message });
+      }
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error?.message,
+    });
+  }
+};
+
+// Update User
+
+userController.updateUser = (req, res) => {
+  try {
+    if (!ObjectId.isValid(req.params.id))
+      return res.status(404).send({
+        message: `No Record with given id:${req.params.id}`,
+        data: "",
+      });
+    const user = {
+      name: req.body.name,
+      mobileNo: req.body.mobileNo,
+      email: req.body.email,
+      location: req.body.location,
+      pinCode: req.body.pinCode,
+    };
+    User.findByIdAndUpdate(
+      req.params.id,
+      { $set: user },
+      { new: true },
+      (err, doc) => {
+        if (!err) {
+          res.send({ message: "success", data: doc });
+        } else {
+          res.send({ message: "Failed", data: "", error: err.message });
+        }
+      }
+    );
+  } catch (error) {
+    res.status(500).send({
+      message: error?.message,
+    });
+  }
 };
 
 // Remove user
 
 userController.removeUser = (req, res) => {
-  if (!ObjectId.isValid(req.params.id))
-    return res.status(400).send(`No Record with given id:${req.params.id}`);
-  User.findByIdAndRemove(req.params.id, (err, doc) => {
-    if (!err) {
-      res.send({ message: "success", data: doc });
-    } else {
-      res.send({ message: "Failed", data: "", error: err.message });
-    }
-  });
+  try {
+    if (!ObjectId.isValid(req.params.id))
+      return res.status(404).send({
+        message: `No Record with given id:${req.params.id}`,
+        data: "",
+      });
+    User.findByIdAndRemove(req.params.id, (err, doc) => {
+      if (!err) {
+        res.send({ message: "success", data: doc });
+      } else {
+        res.send({ message: "Failed", data: "", error: err.message });
+      }
+    });
+  } catch (error) {
+    res.status(500).send({
+      message: error?.message,
+    });
+  }
 };
 
 module.exports = userController;
